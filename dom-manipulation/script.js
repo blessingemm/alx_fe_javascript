3e184ea60e6296005d3c220c53ff6cecb66f90e5
Blessing <eblessing793577@gmail.com>
2025-06-28 12:04:53 +0100

    Implement web storage and JSON handling

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -9,14 +9,32 @@ const newQuoteBtn = document.getElementById('newQuote');
 const selectCategory = document.getElementById('categoryFilter')
 const filterBtn = document.getElementById('filterQuote')
 const containerForm = document.getElementById('formContainer')
+const exportBtn = document.getElementById('exportQuotes')
+
+function loadQuotes(){
+  const storedQuotes = localStorage.getItem('quotes');
+  if(storedQuotes){
+    quotes = JSON.parse(storedQuotes);
+  }else{
+    let quotes = [
+    { text: "The best way to get started is to quit talking and begin doing.", category: "Motivation" },
+    { text: "Life is what happens when you're busy making other plans.", category: "Life" },
+    { text: "Believe you can and you're halfway there.", category: "Inspiration" }
+  ];
+  saveQuotes()
+  }
+}
+
+function saveQuotes(){
+ localStorage.setItem('quotes', JSON.stringify(quotes)); 
+}
 
 function showRandomQuote(){
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const quote = quotes[randomIndex]
   displayQuotes.innerHTML = `"<em>${quote.text}</em>" - (${quote.category})`;
-
+  console.log(sessionStorage.setItem('lastQuote', JSON.stringify(quote)))
 }
-
 
 function showRandomQuoteByCategory(){
   const category = selectCategory.value.trim()
@@ -32,6 +50,7 @@ function showRandomQuoteByCategory(){
   const randomIndex = Math.floor(Math.random() * filtered.length);
   const quote = filtered[randomIndex]
   displayQuotes.innerHTML = `"<em>${quote.text}</em>" - (${quote.category})`;
+  sessionStorage.setItem('lastQuote', JSON.stringify(quote))
 }
 
 
@@ -53,7 +72,9 @@ function createAddQuoteForm(){
   containerForm.appendChild(textInput);
   containerForm.appendChild(categoryInput);
   containerForm.appendChild(addQuoteBtn)
+
 }
+
 function addQuote(){
   const textInput = document.getElementById('newQuoteText')
   const categoryInput = document.getElementById('newQuoteCategory')
@@ -68,6 +89,43 @@ function addQuote(){
     alert('Please, enter a quote and a category')
   }
 }
+
+function exportToJsonFile(){
+  const json = JSON.stringify(quotes, null, 2);
+  const blob = new Blob([json], { type: 'application/json' });
+  const url = URL.createObjectURL(blob);
+  console.log(url)
+
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'quotes.json'
+  a.click();
+
+
+  URL.revokeObjectURL(url);
+}
+
+function importFromJsonFile(event){
+  const fileReader = new FileReader();
+  fileReader.onload = function(event) {
+    try {
+      const importedQuotes = JSON.parse(event.target.result);
+      if(Array.isArray(importedQuotes)) {
+        quotes.push(...importedQuotes);
+        saveQuotes();
+        alert('Quotes imported sucessfully')
+      }else {
+        alert('Invalid file format');
+      }
+    } catch (e) {
+      alert('Error reading file');
+    }
+  };
+  fileReader.readAsText(event.target.files[0]);
+}
+
+loadQuotes();
+createAddQuoteForm();
 newQuoteBtn.addEventListener('click', showRandomQuote)
 filterBtn.addEventListener('click', showRandomQuoteByCategory)
-createAddQuoteForm()
+exportBtn.addEventListener('click', exportToJsonFile)
